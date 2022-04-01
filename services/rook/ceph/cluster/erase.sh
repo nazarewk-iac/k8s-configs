24ca12d7907628de08063449f7d3bd6dbb747d30
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-04-02 00:11:05 +0200

    WIP: fixing up Ceph

--- services/rook/ceph/cluster/erase.sh
+++ services/rook/ceph/cluster/erase.sh
@@ -3,4 +3,15 @@ set -xeEuo pipefail
 shopt -s nullglob
 
 cd "${BASH_SOURCE[0]%/*}"
-rm -rf /var/lib/rook/rook-ceph /var/lib/mon-*
+
+q(){
+  yq "$@" chart.yaml
+}
+kustomize build --enable-helm . | kubectl delete -f -
+kubectl patch -n rook-ceph cephblockpools.ceph.rook.io --patch='{"metadata":{"finalizers":[]}}'
+if test "$(hostname)" = "nazarewk-krul"; then
+  rm -rf /var/lib/rook
+  for device in $(q -r '.valuesInline.cephClusterSpec.storage.devices[].fullpath') ; do
+    dd if=/dev/zero of="$device" bs=1M count=10
+  done
+fi
