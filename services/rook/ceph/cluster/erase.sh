73c49bcd70ba498f21efa721ff60c600b1b7b707
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-04-05 07:51:29 +0200

    Rook Ceph working. TODO: nvme incorrectly registered as ssd

--- services/rook/ceph/cluster/erase.sh
+++ services/rook/ceph/cluster/erase.sh
@@ -4,10 +4,8 @@ shopt -s nullglob
 
 cd "${BASH_SOURCE[0]%/*}"
 
-q(){
-  yq "$@" chart.yaml
-}
 kustomize build --enable-helm . | kubectl delete --force --wait=false --ignore-not-found -f -
+
 blockpools=(
   $(kubectl get -n rook-ceph cephblockpools.ceph.rook.io -o name)
 )
@@ -15,8 +13,10 @@ if test "${#blockpools[@]}" -gt 0 ; then
   kubectl patch -n rook-ceph "${blockpools[@]}" --type=merge --patch='{"metadata":{"finalizers":[]}}'
 fi
 if test "$(hostname)" = "nazarewk-krul"; then
-  rm -rf "$(q -r '.valuesInline.cephClusterSpec.dataDirHostPath')"
-  for device in $(q -r '.valuesInline.cephClusterSpec.storage.devices[].fullpath') ; do
-    ceph-volume lvm zap "$device"
+  for disk in /dev/disk/by-id/dm-name-rook-ceph-* ; do
+    cryptsetup close "${disk}"
   done
+  sync
+  rm -rf "$(yq -r '.valuesInline.cephClusterSpec.dataDirHostPath' chart.yaml)"
+  ceph-volume lvm zap $(yq -r '.spec.local.path | select(.)' pvs.yaml)
 fi
